7ea714cfcb40a602e335c97dbfa208acf7861e1d
matteo9723 <matteolandri7@gmail.com>
2021-11-22 16:43:43 +0100

    esercizio base completato

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log('ciao');
+const app = new Vue({
+  el: '#app',
+
+  data:{
+    mails:[],
+    loading:true,
+  },
+
+  
+  methods:{
+    printMail(){
+    
+      for(let i = 0 ; i < 10 ; i++){
+         
+       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response)=> {
+         
+         
+
+          let data = response.data;
+          console.log(data);
+          this.mails.push(data.response);      
+          console.log(this.mails);
+          
+        })
+        .catch((error)=> {
+          
+          console.log(error);
+        });
+      }; 
+        
+    }
+  },
+  
+  mounted(){
+    this.printMail();
+  },
+
+});
